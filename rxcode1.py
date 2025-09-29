af0472c7dbb51fe1a64cfd02828fbb69350a4f10
rxcoder-bd <rxcoderbd@gmail.com>
2025-09-29 14:13:22 +0600

    Delete rxcode1.py

--- rxcode1.py
+++
@@ -1,5 +0,0 @@
-print("Hello World") #print Hello World
-
-print("Ratul") # print ratul name 
-
-print(" A story is a narrative that describes connected events or experiences, which can be either real or imagined and told for various purposes such as entertainment, education, or cultural preservation. Stories are a fundamental part of human communication, shared orally, in writing, or through other media like film and theater. They can range from short tales and anecdotes to longer novels and plays, and also include reports of events, like a news story, or even a lie or falsehood ") #Printing A Story 
